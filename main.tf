e8462dd420f2b718956dbd700d82d919ffa7a2b5
Marcelo Oliveira <marcelo@marceloweb.info>
2020-07-14 07:56:44 -0300

    - main

--- main.tf
+++ main.tf
@@ -6,16 +6,16 @@ provider "aws" {
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraform"
-    key            = "${var.nome-tfstate}"
-    region         = "sa-east-1"
+    bucket         = "xpto-terraform-example"
+    key            = "example-s3"
+    region         = "us-east-1"
   }
 }
 
 # Criação da instancia - Master
 resource "aws_instance" "storage-master" {
   ami                         = "ami-0bd01eb1d7433538b"
-  instance_type               = "c5.xlarge"
+  instance_type               = "t2.micro"
   key_name                    = "mdcs-docker"
   disable_api_termination = "true"
   ebs_optimized               = "false"
@@ -26,37 +26,37 @@ resource "aws_instance" "storage-master" {
 
 
   tags = {
-      Name = "${var.master-name}"
+      Name = var.master-name
       Terraform = "sim"
       Type-Storage = "master"
       Environment = "PD"
-      Customer = "PepsiCo MEX"
+      Customer = "Example"
   }
 }
- # cria instancia slave
+
+# cria instancia slave
 resource "aws_instance" "storage-slave" {
   ami                         = "ami-0fd035f0b10426dcf"
-  instance_type               = "c5.xlarge"
+  instance_type               = "t2.micro"
   key_name                    = "mdcs-docker"
   disable_api_termination = "true"
   ebs_optimized               = "false"
   availability_zone  = "us-east-1b"
   associate_public_ip_address = "false"
   vpc_security_group_ids = ["sg-0e420d113bbdcd40a"]
-  subnet_id = ""
-  
+  subnet_id = "" 
 
   tags = {
       Name = "${var.slave-name}"
       Terraform = "sim"
       Type-Storage = "slave"
       Environment = "PD"
-      Customer = "PepsiCo MEX"
+      Customer = "Example"
   }
 }
 
 resource "aws_lb" "nlb-storage" {
-  name               = "${var.nlb-name}"
+  name               = var.nlb-name
   internal           = true
   load_balancer_type = "network"
   subnets            = ["subnet", ""]
@@ -65,13 +65,13 @@ resource "aws_lb" "nlb-storage" {
 
   tags = {
     Environment = "PD"
-    Name = "${var.nlb-name}"
-    Customer = "PepsiCo MEX"
+    Name = var.nlb-name
+    Customer = "Example"
   }
 }
 
 resource "aws_lb_target_group" "nlb-target-group" {
-  name     = "${var.tg-name}"
+  name     = var.tg-name
   port     = 445
   protocol = "TCP"
   target_type = "ip"
@@ -87,31 +87,32 @@ resource "aws_lb_target_group" "nlb-target-group" {
 }
 
 resource "aws_lb_listener" "nlb-listener" {
-  load_balancer_arn = "${aws_lb.nlb-storage.arn}"
+  load_balancer_arn = aws-lb.nlb-storage.arn
   port              = "445"
   protocol          = "TCP"
 
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.nlb-target-group.arn}"
+    target_group_arn = aws_lb_target_group.nlb-target-group.arn
   }
 }
 
 resource "aws_lb_target_group_attachment" "storage-tg-attachment" {
-  target_group_arn = "${aws_lb_target_group.nlb-target-group.arn}"
-  target_id        = "${aws_instance.storage-master.private_ip}" 
+  target_group_arn = aws_lb_target_group.nlb-target-group.arn
+  target_id        = aws_instance.storage-master.private_ip 
   port             = 445
 }
 
 resource "aws_lb_target_group_attachment" "storage-tg-attachment-slave" {
-  target_group_arn = "${aws_lb_target_group.nlb-target-group.arn}"
-  target_id        = "${aws_instance.storage-slave.private_ip}" 
+  target_group_arn = aws_lb_target_group.nlb-target-group.arn
+  target_id        = aws_instance.storage-slave.private_ip 
   port             = 445
 }
 
-resource "aws_route53_record" "storage-pepsico" {
+resource "aws_route53_record" "storage-example" {
   zone_id = "/hostedzone/"
-  name    = "${var.route-name}"
+  name    = var.route-name
   type    = "CNAME"
   ttl     = "300"
-  records = ["${aws_lb.nlb-storage.dns_name}"]
+  records = [aws_lb.nlb-storage.dns_name]
+}
---
+++ vars.tf
@@ -0,0 +1,24 @@
+variable "slave-name" {
+  type    = string
+  default = "example-slave"
+}
+
+variable "nlb-name" {
+  type    = string
+  default = "example-nlb"
+}
+
+variable "tg-name" {
+  type    = string
+  default = "example-target"
+}
+
+variable "nlb-listener" {
+  type    = string
+  default = "example-listener"
+}
+
+variable "nlb-storage" {
+  type    = string
+  default = "example-storage"
+}
